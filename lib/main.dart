7e5cc940328def99d42b3b592ad5c13a41fb4d9e
sirine ayadi <sirine.ayadi@enetcom.u-sfax.tn>
2025-11-09 19:52:54 +0100

    Connecting Firebase to the Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/firebase_options.dart';
 import 'package:frontend/signup_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
